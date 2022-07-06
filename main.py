b5eeae13b22c3c6f5105b6d45bcb626fbd3580b8
Dallas City <dallas@unnotechmanila.com>
2022-07-06 11:38:26 +0800

    Implement input experience

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class ToyBot:
 
 	x_axis = list(range(0, 5))
@@ -20,8 +23,8 @@ class ToyBot:
 			- e.g. NORTH, SOUTH, EAST, WEST
 			- note: West is left as East is righ
 		"""
-		self.current_x_pos = x
-		self.current_y_pos = y
+		self.current_x_pos = int(x)
+		self.current_y_pos = int(y)
 		self.current_facing = facing.upper()
 
 	def move(self):
@@ -50,23 +53,34 @@ class ToyBot:
 			  f'{self.current_facing}')
 
 
-# TEST 1
 bot = ToyBot()
-bot.place(0, 0, 'NORTH')
-bot.move()
-bot.report()
+starting_place = input('STARTING PLACE(e.g. 0,0,NORTH): ')
+place = starting_place.replace(' ', '').split(',')
+bot.place(place[0], place[1], place[2])
+next_action = ''
+while next_action.lower() != 'report':
+	next_action = input(
+		'PLEASE INPUT NEXT ACTION(e.g. MOVE, REPORT, LEFT, RIGHT): '
+	)
+	bot.actions[next_action.lower()]()
 
-# TEST 2
-bot = ToyBot()
-bot.place(0, 0, 'NORTH')
-bot.left()
-bot.report()
-
-# TEST 3
-bot = ToyBot()
-bot.place(1, 2, 'EAST')
-bot.move()
-bot.move()
-bot.left()
-bot.move()
-bot.report()
+# # TEST 1: 0,1,NORTH
+# bot = ToyBot()
+# bot.place(0, 0, 'NORTH')
+# bot.move()
+# bot.report()
+#
+# # TEST 2: 0,0,WEST
+# bot = ToyBot()
+# bot.place(0, 0, 'NORTH')
+# bot.left()
+# bot.report()
+#
+# # TEST 3: 3,3,NORTH
+# bot = ToyBot()
+# bot.place(1, 2, 'EAST')
+# bot.move()
+# bot.move()
+# bot.left()
+# bot.move()
+# bot.report()
