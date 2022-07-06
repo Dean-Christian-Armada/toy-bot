c4ee6492df38e993266ec9ebc074928fb23dead7
Dallas City <dallas@unnotechmanila.com>
2022-07-06 15:57:03 +0800

    Implement validations

--- main.py
+++ main.py
@@ -15,6 +15,26 @@ class ToyBot:
 		self.current_y_pos = 0
 		self.current_facing = ''
 
+	def validate_place(self, x, y, facing):
+		valid = False
+		facings = ['NORTH', 'SOUTH', 'WEST', 'EAST']
+		if not x.isdigit():
+			print(f'First value must be a valid number. You entered {x}')
+		elif int(x) >= len(self.x_axis):
+			print(f'First value must be less than {len(self.x_axis)} or else '
+				  f'the bot will fall. You entered {x}')
+		elif not y.isdigit():
+			print(f'Second value must be a valid number. You entered {y}')
+		elif int(y) >= len(self.y_axis):
+			print(f'Second value must be less than {len(self.y_axis)} or else '
+				  f'the bot will fall. You entered {y}')
+		elif facing.upper() not in facings:
+			print(f'Third value is invalid. Choices are {",".join(facings)}. '
+				  f'You entered {facing}')
+		else:
+			valid = True
+		return valid
+
 	def place(self, x, y, facing):
 		"""
 		:param x: initial position on the x-axis
@@ -23,9 +43,12 @@ class ToyBot:
 			- e.g. NORTH, SOUTH, EAST, WEST
 			- note: West is left as East is righ
 		"""
+		if not self.validate_place(x, y, facing):
+			return False
 		self.current_x_pos = int(x)
 		self.current_y_pos = int(y)
 		self.current_facing = facing.upper()
+		return True
 
 	def move(self):
 		if self.current_facing in ['NORTH', 'SOUTH']:
@@ -53,16 +76,27 @@ class ToyBot:
 			  f'{self.current_facing}')
 
 
+print('====TOYBOT CONSOLE APPLICATION====')
 bot = ToyBot()
-starting_place = input('STARTING PLACE(e.g. 0,0,NORTH): ')
-place = starting_place.replace(' ', '').split(',')
-bot.place(place[0], place[1], place[2])
+placed = False
+while not placed:
+	starting_place = input('STARTING PLACE(e.g. 0,0,NORTH): ')
+	place = starting_place.replace(' ', '').split(',')
+	placed = bot.place(place[0], place[1], place[2])
 next_action = ''
 while next_action.lower() != 'report':
 	next_action = input(
-		'PLEASE INPUT NEXT ACTION(e.g. MOVE, REPORT, LEFT, RIGHT): '
+		'PLEASE INPUT NEXT ACTION(e.g. PLACE, MOVE, REPORT, LEFT, RIGHT): '
 	)
-	bot.actions[next_action.lower()]()
+	_next_action = next_action.lower()
+	if _next_action == 'place':
+		starting_place = input('IMMEDIATE PLACING(e.g. 0,0,NORTH): ')
+		place = starting_place.replace(' ', '').split(',')
+		placed = bot.place(place[0], place[1], place[2])
+	elif bot.actions.get(_next_action):
+		bot.actions[next_action.lower()]()
+	else:
+		print(f'You have input a wrong action: {next_action}')
 
 # # TEST 1: 0,1,NORTH
 # bot = ToyBot()
